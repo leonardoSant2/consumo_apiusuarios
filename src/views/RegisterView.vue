a48dfa985bc4a33c5f8e627f92feef53cce4d4b4
Leonardo Santiago <leonardo201220@gmail.com>
2024-04-03 09:50:47 -0300

    Formulário de cadastro

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>REGISTRO DE USUÁRIOS</h1>
+    <hr>
+    <div class="columns is-centered">
+  <div class="column is-half">
+    <p>Nome</p>
+    <input type="text" placeholder="Nome do Usuário" class="input is-rounded is-primary" >
+    <p>E-mail</p>
+    <input type="email" placeholder="email@email.com" class="input is-rounded is-primary">
+    <p>Senha</p>
+    <input type="password" placeholder="******" class="input is-rounded is-primary">
+    <hr>
+    <button class="button is-primary is-fullwidth">Cadastrar</button>
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
